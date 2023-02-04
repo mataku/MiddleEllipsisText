db547e8ab40026ebf9340e657188de3258837b1b
Takuma Homma <nagomimatcha@gmail.com>
2023-02-04 17:27:53 +0900

    No need to add ellipsisChar

--- MiddleEllipsisText/src/main/java/io/github/mataku/middleellipsistext/MiddleEllipsisText.kt
+++ MiddleEllipsisText/src/main/java/io/github/mataku/middleellipsistext/MiddleEllipsisText.kt
@@ -39,7 +39,7 @@ fun MiddleEllipsisText(
 ) {
   if (text.isEmpty()) {
     Text(
-      text = text + ellipsisChar,
+      text = text,
       color = color,
       fontSize = fontSize,
       fontStyle = fontStyle,
