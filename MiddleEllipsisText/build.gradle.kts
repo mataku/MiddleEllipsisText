91e15958fab933f3c3c4c2452d2b6c53a5efef64
Takuma Homma <nagomimatcha@gmail.com>
2023-02-05 23:42:18 +0900

    Apply Gradle convention plugins

--- MiddleEllipsisText/build.gradle.kts
+++ MiddleEllipsisText/build.gradle.kts
@@ -1,44 +1,15 @@
 plugins {
-  id("com.android.library")
-  id("org.jetbrains.kotlin.android")
+  id("middleellipsistext.android.library")
+  id("middleellipsistext.android.compose")
+  id("middleellipsistext.android.test")
   id("maven-publish")
   signing
 }
 
 android {
-  compileSdk = 33
-
-  defaultConfig {
-    minSdk = 24
-    targetSdk = 32
-
-    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    consumerProguardFile("consumer-rules.pro")
-  }
-
-  buildTypes {
-    release {
-      isMinifyEnabled = false
-      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-    }
-  }
-  compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
-  }
-  kotlinOptions {
-    jvmTarget = "1.8"
-  }
-  buildFeatures {
-    compose = true
-  }
-  composeOptions {
-    kotlinCompilerExtensionVersion = libs.versions.compose.kotlin.compiler.extension.get()
-  }
   testOptions {
-    unitTests.isIncludeAndroidResources = true
     managedDevices {
-      devices.create<com.android.build.api.dsl.ManagedVirtualDevice>("pixel4Api31") {
+      devices.maybeCreate<com.android.build.api.dsl.ManagedVirtualDevice>("pixel4Api31").apply {
         device = "Pixel 4"
         apiLevel = 31
         systemImageSource = "aosp"
