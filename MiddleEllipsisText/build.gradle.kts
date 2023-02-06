67e915c8f621210819142f84257b5a45b2e60658
Takuma Homma <nagomimatcha@gmail.com>
2023-02-06 22:10:32 +0900

    v0.0.3

--- MiddleEllipsisText/build.gradle.kts
+++ MiddleEllipsisText/build.gradle.kts
@@ -54,7 +54,7 @@ afterEvaluate {
       create<MavenPublication>("maven") {
         groupId = "io.github.mataku"
         artifactId = libName
-        version = "0.0.2"
+        version = "0.0.3"
         if (project.plugins.hasPlugin("com.android.library")) {
           from(components["release"])
         } else {
