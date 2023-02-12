59824e441010b94f31afb51b40a4f9c11cd9a713
Takuma Homma <nagomimatcha@gmail.com>
2023-02-12 18:09:46 +0900

    TODO

--- MiddleEllipsisText/src/androidTest/java/io/github/mataku/middleellipsistext/TestHelper.kt
+++ MiddleEllipsisText/src/androidTest/java/io/github/mataku/middleellipsistext/TestHelper.kt
@@ -1,15 +1,16 @@
 package io.github.mataku.middleellipsistext
 
 import android.graphics.Bitmap
+import androidx.test.platform.app.InstrumentationRegistry
 import java.io.FileOutputStream
 
 object TestHelper {
   fun saveScreenshot(filename: String, bmp: Bitmap) {
-    if (System.getenv("CI") != null) {
-      return
-    }
+//    if (System.getenv("CI") != null) {
+//      return
+//    }
 
-    val path = "/sdcard"
+    val path = InstrumentationRegistry.getInstrumentation().targetContext.filesDir.canonicalPath
     FileOutputStream("$path/$filename.png").use { out ->
       bmp.compress(Bitmap.CompressFormat.PNG, 100, out)
     }
