b866b255e591249bca45761bcd254503173b7eec
Takuma Homma <nagomimatcha@gmail.com>
2024-04-15 13:37:32 +0900

    :wave: unused lib

--- MiddleEllipsisText/build.gradle.kts
+++ MiddleEllipsisText/build.gradle.kts
@@ -13,7 +13,6 @@ kotlin {
         implementation(compose.foundation)
         implementation(compose.material)
         implementation(compose.ui)
-        implementation(libs.skiko)
       }
     }
 
