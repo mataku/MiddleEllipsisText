6277f74f55f4fcdf327232d9a208f9fcbc30ee04
Takuma Homma <nagomimatcha@gmail.com>
2024-06-25 23:45:01 +0900

    Update screenshots

--- sampleApp/src/commonMain/kotlin/App.kt
+++ sampleApp/src/commonMain/kotlin/App.kt
@@ -1,10 +1,8 @@
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -12,13 +10,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.github.mataku.middleellipsistext3.MiddleEllipsisText
-import middleellipsistext.sampleapp.generated.resources.Res
-import middleellipsistext.sampleapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -26,24 +22,30 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 fun App() {
   MaterialTheme {
     var showContent by remember { mutableStateOf(false) }
-    Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-      Button(onClick = { showContent = !showContent }) {
-        Text("Click me!")
-      }
+    Column(Modifier.fillMaxWidth()) {
+//      AnimatedVisibility(showContent) {
+//        val greeting = remember { Greeting().greet() }
+//        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+//          Image(painterResource(Res.drawable.compose_multiplatform), null)
+//          Text("Compose: $greeting")
+//        }
+//      }
 
-      AnimatedVisibility(showContent) {
-        val greeting = remember { Greeting().greet() }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-          Image(painterResource(Res.drawable.compose_multiplatform), null)
-          Text("Compose: $greeting")
-        }
-      }
+      Text(
+        text = "Ellipsized text",
+        fontSize = 24.sp,
+        modifier = Modifier.padding(16.dp),
+        fontWeight = FontWeight.Bold
+      )
 
-      Spacer(modifier = Modifier.height(32.dp))
+      Spacer(modifier = Modifier.height(16.dp))
 
       MiddleEllipsisText(
-        text = "This is a looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong text that will be truncated in the middle",
-        modifier = Modifier.fillMaxWidth(),
+        text = "This is a looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong text that will be truncated in the middleツ",
+        modifier = Modifier.fillMaxWidth()
+          .padding(
+            horizontal = 16.dp
+          ),
       )
     }
   }
